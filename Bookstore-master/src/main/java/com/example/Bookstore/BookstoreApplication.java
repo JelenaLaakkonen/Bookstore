52a0e8c5cf6b78179764a98e6099a253c02a2943
jelenalaakkonen <bgm039@myy.haaga-helia.fi>
2021-03-10 22:01:44 +0200

    Adding new method...

--- Bookstore-master/src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ Bookstore-master/src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
 import com.example.Bookstore.domain.Category;
 import com.example.Bookstore.domain.CategoryRepository;
+import com.example.Bookstore.domain.User;
+import com.example.Bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -17,21 +20,26 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(BookRepository repository, CategoryRepository cRepository) {
+	public CommandLineRunner demo(BookRepository repository, CategoryRepository cRepository, UserRepository uRepository) {
 		return (args) -> {
-			
+
 			Category c1 = new Category("Elämänkerta");
 			Category c2 = new Category("Jännitys");
-			
+
 			cRepository.save(c1);
 			cRepository.save(c2);
-			
+
 			Book book1 = new Book(c1, "Papan kanssa kahvilla", "Lotta-Sofia Saahko", 2020, "0000001", 14.90);
-			Book book2 = new Book(c2 ,"Suon villi laulu", "Delia Owens", 2020, "0000002", 14.90);
+			Book book2 = new Book(c2, "Suon villi laulu", "Delia Owens", 2020, "0000002", 14.90);
 
 			repository.save(book1);
 			repository.save(book2);
 
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "user@user.fi","USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "admin@admin.fi", "ADMIN");
+			
+			uRepository.save(user1);
+			uRepository.save(user2);
 		};
 	}
 
